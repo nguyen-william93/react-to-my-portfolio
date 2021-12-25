e2d7f7c7e53d80c796a916f1ff1882a332c27267
nguyen-william93 <=>
2021-12-25 15:49:53 -0600

    inital structure for header and nav

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header(props){
+    const {currentTab, setTab} = props;
+
+    return (
+        <header>
+            <div>
+                <h2>William Nguyen React Portfolio</h2>
+            </div>
+            <div>
+                <Nav currentTab = {currentTab} setTab={setTab}></Nav>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav(props){
+    const { currentTab, setTab } = props;
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </nav>
+    )
+}
